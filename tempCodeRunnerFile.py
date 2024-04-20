3a8060db86c9444ffd2f379e9b6a4fc029f62c3f
Erik Dubois <plackovicdajan@gmail.com>
2024-04-20 09:36:43 +0100

    fix: remove templ file I accidentally created

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-                    # self.chain_ends = [chain_end for chain_end in
-                    #                    self.chain_ends if chain_end["point"] !=
-                    #                    tuple(starting_point) and
-                    #                    chain_end["end"]
-                    #                    != dir_complements[direction]]
